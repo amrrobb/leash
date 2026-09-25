f4207684abd322705c0d61792a781bd09e850234
Ammar Robbani <amr.robb@gmail.com>
2026-09-26 03:34:26 +0900

    test: MockEAC mirrors ENSv2 id versioning and adds roles()

--- test/mocks/MockEAC.sol
+++ test/mocks/MockEAC.sol
@@ -3,46 +3,48 @@ pragma solidity 0.8.30;
 
 import { IEAC } from "../../src/interfaces/IEAC.sol";
 
-/// @notice One-name stand-in for an ENSv2 PermissionedRegistry.
-/// Mirrors the two behaviours Vault depends on: token ids change on every grant/revoke,
-/// and hasRoles still accepts any id ever issued for the name.
+/// @notice One-name stand-in for an ENSv2 PermissionedRegistry, modelled on its source:
+/// token id = keccak256(label) with the low 32 bits replaced by a version that bumps on every
+/// grant/revoke (_regenerate), and role reads accept any version of the id (getResource).
 contract MockEAC is IEAC {
-    bytes32 public immutable labelHash;
-    uint256 public version;
-    mapping(address account => uint256 roles) public roles;
-    mapping(uint256 id => bool) public issued;
+    uint256 public immutable labelId;
+    uint32 public version;
+    mapping(address account => uint256 roles) internal _roles;
 
     constructor(string memory label) {
-        labelHash = keccak256(bytes(label));
-        issued[_id()] = true;
+        labelId = uint256(keccak256(bytes(label)));
     }
 
     function grantRoles(uint256 roleBitmap, address account) external {
-        roles[account] |= roleBitmap;
-        _bump();
+        _roles[account] |= roleBitmap;
+        version++;
     }
 
     function revokeRoles(uint256 roleBitmap, address account) external {
-        roles[account] &= ~roleBitmap;
-        _bump();
+        _roles[account] &= ~roleBitmap;
+        version++;
     }
 
-    function hasRoles(uint256 resource, uint256 roleBitmap, address account) external view returns (bool) {
-        require(issued[resource], "unknown token id");
-        return roles[account] & roleBitmap == roleBitmap;
+    function hasRoles(uint256 anyId, uint256 roleBitmap, address account) external view returns (bool) {
+        _check(anyId);
+        return _roles[account] & roleBitmap == roleBitmap;
+    }
+
+    function roles(uint256 anyId, address account) external view returns (uint256) {
+        _check(anyId);
+        return _roles[account];
     }
 
     function findTokenId(string calldata label) external view returns (uint256) {
-        require(keccak256(bytes(label)) == labelHash, "unknown label");
-        return _id();
+        require(uint256(keccak256(bytes(label))) == labelId, "unknown label");
+        return _withVersion(labelId, version);
     }
 
-    function _id() internal view returns (uint256) {
-        return (uint256(labelHash) << 32) | version;
+    function _check(uint256 anyId) internal view {
+        require(_withVersion(anyId, 0) == _withVersion(labelId, 0), "unknown token id");
     }
 
-    function _bump() internal {
-        version++;
-        issued[_id()] = true;
+    function _withVersion(uint256 anyId, uint32 v) internal pure returns (uint256) {
+        return anyId ^ uint32(anyId) ^ v;
     }
 }
